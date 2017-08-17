7cc0562fceec1afc0ec8dd20e7cd4a4ff94d1849
Daniel Muller <DMuller@DM.cfl.rr.com>
2017-08-17 06:55:53 -0400

    CSS Modify

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # week-4-game
+
+This game is called Crystal Collector
+
+The value of each crystal is a mystery until you click them.
+
+Each time you click a crystal, its value is added to your total. 
+
+If you can get the total values of each Crystal to equate to the random generated number, you win!!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
 	var numWins = 0
 	var numLosses = 0
 	var counter = 0
-
 	var targetNum = Math.floor(Math.random()*100+20);
 
 
@@ -13,17 +12,15 @@ $(document).ready(function() {
 	function getRandomNum(min, max) {
     	return Math.random() * (max - min) + min;
     }
-		var numOne = getRandomNum(1,3);
-		var numTwo = getRandomNum(4,8);
-		var numThree = getRandomNum(9,12);
-		var numFour = getRandomNum(13,15);
-
+		var numOne = getRandomNum(1,5);
+		var numTwo = getRandomNum(5,10);
+		var numThree = getRandomNum(11,15);
+		var numFour = getRandomNum(13,20);
 
 console.log("numOne: " + numOne);
 console.log("numTwo: "+ numTwo);
 console.log("numThree: " + numThree);
 console.log("numFour: " + numFour);
-
 
 	var numOptions = [numOne, numTwo, numThree, numFour];
 
@@ -38,7 +35,6 @@ console.log("numFour: " + numFour);
 
 	// var targetNumChg = numOptions[Math.round(Math.random()*numOptions.length)];
 	
-
 	function startGame(){
 		for (i = 0; i < numOptions.length; i++) {
 			var imgCrystal = $("<img>");	
@@ -46,29 +42,7 @@ console.log("numFour: " + numFour);
 			//gives each crystal a value from above array.//MODIFY FOR RANDOM????
 			$(".crystalPics").append(imgCrystal);
 		}
-		$(".crystal-image").on("click", function() {	
-			var crystalValue = ($(this).attr("data-crystalvalue"));
-			crystalValue = parseInt(crystalValue);
-				//Adding a counter, increasing by the crystal's value
-			counter += crystalValue;
-		    	// Clicking the button triggers an alert message.
-		    	//If the counter increments up to the Target Number = You win!  
-		    $("#myScoreNum").html(counter);
-		    $("#randNum").html("Random Number: " + targetNum);
-		    updateDataWinLose();
-		});
 	}
-
-	//reset the values to new random numbers
-	function resetGame(){
-		counter = 0;
-		targetNum = Math.floor(Math.random()*100+20);
-		numOne = getRandomNum(1,10);
-		numTwo = getRandomNum(6,10);
-		numThree = getRandomNum(11,15);
-		numFour = getRandomNum(16,20);
-	}
-
 	//After win/loss, this resets data:
 	function updateDataWinLose() {
 		if (targetNum === counter) {
@@ -76,24 +50,37 @@ console.log("numFour: " + numFour);
 			alert("You win, way to go!");
 			$("#wins").html("Wins: " + numWins);
 			resetGame();
-
-
 		} else if (counter >= targetNum) {
 	      	alert("You lost, try again.");
 	      	numLosses++;
-	      	$("#losses").html("Losses: " + numLosses);	  
+	      	$("#losses").html("Losses: " + numLosses);	  		      	
 	      	resetGame();	
 	    }
 	}
 
+//reset the values to new random numbers
+	function resetGame(){
+		counter = 0;
+		targetNum = Math.floor(Math.random()*100+20);
+		numOne = getRandomNum(1,5);
+		numTwo = getRandomNum(5,10);
+		numThree = getRandomNum(11,15);
+		numFour = getRandomNum(13,20);
+	}
+
 	startGame();
-	updateDataWinLose();
 
-
-
-
-	   
-
+	$(".crystal-image").on("click", function() {	
+		var crystalValue = ($(this).attr("data-crystalvalue"));
+		crystalValue = parseInt(crystalValue);
+			//Adding a counter, increasing by the crystal's value
+		counter += crystalValue;
+	    	// Clicking the button triggers an alert message.
+	    	//If the counter increments up to the Target Number = You win!  
+	    $("#myScoreNum").html(counter);
+	    $("#randNum").html("Random Number: " + targetNum);
+	    updateDataWinLose();
+	});
 });
 
 
